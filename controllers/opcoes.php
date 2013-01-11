e179b05d3dbda2d51c99b08f091dba9bfcdbf341
Thiago Padilha <thiago.moura@ibti.net.br>
2013-01-11 13:43:39 -0200

    Controler opções
    Teste do controler opções

--- controllers/opcoes.php
+++ controllers/opcoes.php
@@ -10,9 +10,35 @@ class JSON_API_Opcoes_Controller {
 	 global $json_api;	  
 	 extract($json_api->query->get(array('')));
 	 
+<<<<<<< HEAD
+	 	$opcoes = pods( 'opcoes');
+		$params = array(
+					'orderby' => 't.id DESC'
+				);
+				
+		$opcoes->find($params);
+		$output=array();
+		
+		while ( $opcoes->fetch() ){
+			
+			$arquivo = $opcoes->display( 'icone');
+			$extensao = pathinfo($arquivo, PATHINFO_EXTENSION);
+			
+   			$output[]= 	array('Nome'=> $opcoes->display( 'name'),
+   							'Ordem'=> $opcoes->display( 'ordem'),
+   							'Descricao'=>$opcoes->display( 'descricao'),
+   							'icone'=>$opcoes->display( 'icone'),
+   							'tipo_icone'=>$extensao,
+   							'link'=>$opcoes->display( 'link_interno')
+					);
+		}
+	
+	 	return $output;
+=======
 	 $output = array();
 	 	 
 	 return $output;
+>>>>>>> parent of 50fd9b9... Controller opcoes
 	}	
 } 	
 ?>
