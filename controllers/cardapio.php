403837822b1df482dcfa2865887f16d786682c9b
Thiago Padilha <thiago.moura@ibti.net.br>
2013-01-22 10:16:47 -0200

    Controller Cardapio Funcionalidade search_item

--- controllers/cardapio.php
+++ controllers/cardapio.php
@@ -107,30 +107,45 @@ class JSON_API_Cardapio_Controller {
 	public function search_itens() {
 		global $json_api;
 		extract($json_api->query->get(array('e','k','p')));
-		
-		$itens= pods( 'itens');
-		$params = array(
-						'orderby' => 't.id DESC',
-						'where'=>$ic,
-		);
-		
-		$itens->find($params);
-		$output=array();
-		
-		while ( $itens->fetch() ){
-			
-   			$output[]= 	array('Codigo'=>$itens->display( 'codigo_do_item'),
-   							'Preço'=>$itens->display( 'preco'),
-   							'Foto'=>$itens->display( 'foto_principal'),
-   							'Calorias'=>$itens->display( 'calorias'),
-   							'Layout'=>$itens->display( 'layout_da_tela'),
-   							'Impressora'=>$itens->display( 'impressora'),
-   							'Adicionais'=>$itens->display( 'adicionais_'),
-   							'Retiraveis'=>$itens->display( 'retiraveis'),
-   							'Categoria'=>$itens->display( 'categorias_'),
+		if(is_null($k)){
+			$output=array('erro_1'=>'Palavra chave não informada da situação não informada.');
+		}elseif(is_null($p)){
+			$output=array('erro_2'=>'Preço não informada da situação não informada.');
+		}else{
+			$itens= pods( 'itens');
+			$params = array(
+							'orderby' => 't.id DESC',
+							'where'=>"t.post_content like '%".$k."%'"
+							//'join'=>"INNER JOIN `wp_postmeta` ON `wp_postmeta`.`post_id` = t.ID AND (meta_key='preco') AND ((wp_postmeta.meta_valuea>".($p-1).') AND (wp_postmeta.meta_value<'.($p+1).'))'
+							//esquece join faz a verificação no while precisa converter o valor do preço
 			);
+			
+			$itens->find($params);
+			$output=array();
+			$total=0;
+			$result=array();
+			
+			while ( $itens->fetch() ){
+				$preco=0+str_replace('$','',$itens->display( 'preco'));
+				$p = (is_null($p)) ? $preco : $p;
+				if(($preco<$p+1)&&($preco>$p-1)){
+					$total=$total+1;
+		   			$result[]=array('Codigo'=>$itens->display( 'codigo_do_item'),
+		   							'Preço'=>$itens->display( 'preco'),
+		   							'Foto'=>$itens->display( 'foto_principal'),
+		   							'Calorias'=>$itens->display( 'calorias'),
+		   							'Layout'=>$itens->display( 'layout_da_tela'),
+		   							'Impressora'=>$itens->display( 'impressora'),
+		   							'Adicionais'=>$itens->display( 'adicionais_'),
+		   							'Retiraveis'=>$itens->display( 'retiraveis'),
+		   							'Categoria'=>$itens->display( 'categorias_'),
+					);					
+				}
+			}
+			$output=array_merge(array('Total'=>$total),$result);
 		}
-		$output=array_merge(array('Total'=>$itens->total()),$output);
+		
+	
 		return $output;
 	}
 	
