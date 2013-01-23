0612ed838aeeec7515009825aed5c33a95a08e04
Ricardo Borges <ricardo.borges@ibti.net.br>
2013-01-23 17:55:19 -0200

    Controller Publicidade

--- controllers/publicidade.php
+++ controllers/publicidade.php
@@ -1,4 +1,5 @@
-<?php
+﻿<?php
+
 /*
 Controller name: Publicidade
 Controller description: Oh Garçom/IBTI Publicidade 
@@ -9,19 +10,19 @@ class JSON_API_Publicidade_Controller {
 	public function get_lista_publicidade() {
 	  global $json_api;	 
 	  extract($json_api->query->get(array('e')));
-	  	 	  
+	
 	  function get_img($id){ 		  
   	    $sql_img = "
-			  SELECT DISTINCT `m`.`meta_value`
-			  FROM `wp_postmeta` AS m
-			  WHERE `m`.`post_id` = ( 
-			    SELECT DISTINCT `m`.`meta_value`	        
-			    FROM `wp_posts` AS `t`
-			    INNER JOIN `wp_postmeta` AS `m`
-			    ON `t`.`ID` = `m`.`post_id` 
-			    WHERE (`m`.`meta_key` = '_thumbnail_id') AND (`t`.`id` = ".$id.")
-			  ) AND `meta_key` = '_wp_attached_file'
-             ";	 
+			        SELECT DISTINCT `m`.`meta_value`
+			        FROM `wp_postmeta` AS m
+			        WHERE `m`.`post_id` = ( 
+			          SELECT DISTINCT `m`.`meta_value`	        
+			          FROM `wp_posts` AS `t`
+			          INNER JOIN `wp_postmeta` AS `m`
+			          ON `t`.`ID` = `m`.`post_id` 
+			          WHERE (`m`.`meta_key` = '_thumbnail_id') AND (`t`.`id` = ".$id.")
+			        ) AND `meta_key` = '_wp_attached_file'
+                   ";	 
 				 
 	    return $sql_img;		  
       }	
@@ -49,18 +50,19 @@ class JSON_API_Publicidade_Controller {
 	  	$img = pods('publicidade');
 	  	$img->find(null, 0, null, get_img($publicidade->display('ID'))); 	
 	  	
-	  	$output[] = array('title'           => $publicidade->display('post_title'),
-						  'textoDescritivo' => $publicidade->display('post_content'),							  			  
-						  'imagem'          => $img        ->display('meta_value'),
-						  'data-incicio'	=> $publicidade->display('data_inicio'),	
-						  'data-fim'        => $publicidade->display('data_fim')
+	  	$output[] = array(
+	  		'title'           => $publicidade->display('post_title'),
+			'textoDescritivo' => $publicidade->display('post_content'),							  			  
+			'imagem'          => $img        ->display('meta_value'),
+			'data-incicio'	  => $publicidade->display('data_inicio'),	
+			'data-fim'        => $publicidade->display('data_fim')
 		);	
 		$img = null;			
 	  }  
 	  
-	  $count = $publicidade->total();	 
-	  array_unshift($output, $count); 
-	  	
+	  $count = $publicidade->total();	
+	  $output=array_merge(array('total'=>$count),$output);
+	 	  	  	
 	  return $output;	
 	}
 } 
